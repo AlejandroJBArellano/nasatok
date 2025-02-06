a2d969ae4f17f066312c12742c572cd276c2bdee
Alejandro Arellano <jesusarellano334@gmail.com>
2025-02-05 18:11:25 -0600

    feat: add loading state to Home component while fetching APOD data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,20 +14,30 @@ const Home = () => {
   const apiKey = import.meta.env.VITE_API_KEY as string;
 
   const [data, setData] = useState<IAPOD[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchAPODApi(apiKey).then((data) => setData(data));
+    fetchAPODApi(apiKey).then((data) => {
+      setData(data);
+      setLoading(false);
+    });
   }, [apiKey]);
   return (
     <IonPage>
       <div className="text-center absolute w-full top-0 bg-white/30 backdrop-blur-lg p-1 shadow-lg">
         <h2 className="text-2xl font-bold">Nasatok</h2>
       </div>
-      <div className="snap-y snap-mandatory h-[100dvh] overflow-y-scroll h-screen">
-        {data.map((el) => (
-          <APOD key={el.date} {...el} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="w-10 h-10 border-4 border-t-[4px] border-gray-200 rounded-full animate-spin"></div>
+        </div>
+      ) : (
+        <div className="snap-y snap-mandatory h-[100dvh] overflow-y-scroll h-screen">
+          {data.map((el) => (
+            <APOD key={el.date} {...el} />
+          ))}
+        </div>
+      )}
     </IonPage>
   );
 };
