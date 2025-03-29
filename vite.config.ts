0d702a04fb9478f6d5b01edd654d222e6e7cc42d
Alejandro Arellano <jesusarellano334@gmail.com>
2025-03-28 23:02:55 -0600

    feat: add vite-plugin-vercel for deployment and update package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 /// <reference types="vitest" />
 
-import tailwindcss from '@tailwindcss/vite'
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    legacy()
-  ],
+  plugins: [tailwindcss(), react(), legacy(), vercel()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
